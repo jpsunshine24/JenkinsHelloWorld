f47256892f9e7dd38193f0933478366e8a26630e
jpsunshine24 <88393167+jpsunshine24@users.noreply.github.com>
2023-07-14 14:19:24 -0500

    Update HelloWorld.java
    change to test webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String[] args){
-   System.out.println("----***Hello World and Welcome to Jenkins Training***------");
+   System.out.println("----***Hello World and Welcome to Jenkins Training - Webhook Test***------");
 }
 }
